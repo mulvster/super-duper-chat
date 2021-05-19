ea0388d873b8a8818853e543e0f2aa2a9b0e67be
April Mulvey <april.mulvey@gmail.com>
2021-05-19 15:12:15 -0700

    Sign in and sign out using google auth and firebase complete

--- src/App.js
+++ src/App.js
@@ -19,13 +19,34 @@ const auth = firebase.auth()
 const firestore = firebase.firestore()
 
 function App() {
+  const [user] = useAuthState(auth)
   return (
     <div className="App">
       <header className="App-header">
      
       </header>
+      <section>{ user ? <Chatroom /> : <Signin /> }</section>
     </div>
   )
+}
+function Signin() {
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider()
+    auth.signInWithPopup(provider)
+  }
+  return (
+    <button onClick={signInWithGoogle}>Sign In With Google</button>
+  )
+}
+
+function SignOut() {
+  return auth.currentUser && (
+    <button onClick={() => auth.signOut()}>Sign Out</button>
+  )
+}
+
+function Chatroom() {
+
 }
 
 export default App
