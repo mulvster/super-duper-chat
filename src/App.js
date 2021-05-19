40ac8da22d85f44ddf40213d23d13f99c535049f
April Mulvey <april.mulvey@gmail.com>
2021-05-19 15:02:56 -0700

    More firebase setup

--- src/App.js
+++ src/App.js
@@ -6,7 +6,13 @@ import { useAuthState } from 'react-firebase-hooks/auth'
 import { useCollectionData} from 'react-firebase-hooks/firestore'
 
 firebase.initializeApp({
-
+    apiKey: "AIzaSyBI3E_8900ttRXF-w8yUy_bjG2A3GT9scs",
+    authDomain: "super-duper-chat-5e40f.firebaseapp.com",
+    projectId: "super-duper-chat-5e40f",
+    storageBucket: "super-duper-chat-5e40f.appspot.com",
+    messagingSenderId: "410609379333",
+    appId: "1:410609379333:web:a988acdca243ca80e6146c",
+    measurementId: "G-31M8C9WD8Q"
 })
 
 const auth = firebase.auth()
